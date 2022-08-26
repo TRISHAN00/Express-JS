7f0f708c347a9bd98d4ba343f857ad679e096ddf
TRISHAN00 <trishansaha43@gmail.com>
2022-08-26 12:07:43 +0600

    response json

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello User");
+  res.json({
+    message: "Hello World",
+  });
 });
 
 app.listen(4000, () => {
