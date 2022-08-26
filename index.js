ef9dd5360307e5637d84c0214fae2d801745dade
TRISHAN00 <trishansaha43@gmail.com>
2022-08-26 12:11:19 +0600

    raw html code in express send

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "Hello World",
-  });
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Node Js</title>
+      <style>
+          * {margin: 0; padding: 0; box-sizing: border-box;}
+          body {
+              background-color:gray;
+              color: black;
+          }
+      </style>
+  </head>
+  <body>
+      <h2>Hello Node JS</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, suscipit.</p>
+  </body>
+  </html>
+  `);
 });
 
 app.listen(4000, () => {
