20f09ceba146c073629ff23e825d5c4743594ed2
TRISHAN00 <trishansaha43@gmail.com>
2022-09-25 13:35:35 +0600

    add multi page routes

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-const PORK = process.env.PORK || 8080;
+
+app.get("/about", (req, res) => {
+  res.send(`This is About Page`);
+});
+
+app.get("/contract", (req, res) => {
+  res.send("This is Contract Page");
+});
 
 app.get("/", (req, res) => {
   res.send(`<h2>This is my first Route</h2>`);
 });
 
+app.get("*", (req, res) => {
+  res.send(`<h2>404 Not Found</h2>`);
+});
+
+const PORK = process.env.PORK || 8080;
 app.listen(PORK, () => {
   console.log(`Served is running on pork ${PORK}`);
 });
