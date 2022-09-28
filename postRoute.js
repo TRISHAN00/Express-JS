23f2cd3d1d1c8f51497f15eeb856bd4d69a11d41
TRISHAN00 <trishansaha43@gmail.com>
2022-09-28 14:34:30 +0600

    get category, page, filter from client url

--- postRoute.js
+++ postRoute.js
@@ -1,19 +1,27 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  const { category, page, filter } = req.query;
   res.send("Render all Posts");
 });
 
-router.post("/", (req, res) => {
+router.get("/:postId", (req, res) => {
+  res.send("I am POST = " + req.params.postId);
+});
+
+router.post("/:postId", (req, res) => {
+  console.log(req.params);
   res.send("create new post");
 });
 
-router.put("/", (req, res) => {
-  res.send("Update your existing post");
+router.put("/:postId", (req, res) => {
+  console.log(req.params);
+  res.send("Update your existing post = " + req.params.postId);
 });
 
-router.delete("/", (req, res) => {
-  res.send("Delete your existing post");
+router.delete("/postId", (req, res) => {
+  console.log(req.params);
+  res.send("Delete your existing post = " + req.params.postId);
 });
 
 module.exports = router;
